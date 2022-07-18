c6b71bcaca3c04f1bb2184dc8de3f64910f73db5
sokumsed <109549961+sokumsed@users.noreply.github.com>
2022-07-18 14:57:51 -0400

    Add files via upload

---
+++ PythonApplication1/PythonApplication1.py
@@ -0,0 +1,16 @@
+import time
+import pyautogui as pg
+import sys
+
+
+pg.moveTo(1839, 125)
+i = 0
+
+
+while i < 1:
+    pg.moveTo(1839, 125)
+    pg.mouseDown(); pg.mouseUp()
+    pg.moveTo(1802, 186)
+    pg.mouseDown(); pg.mouseUp()
+    pg.press('y')
+
